77418b91212210b918ec60b936f03b16c6cdb86d
David <david_arobase_mercereau_point_info>
2013-11-04 19:28:30 +0100

    Debut du dev 0.4

--- conf.php
+++ conf.php
@@ -3,50 +3,74 @@
 //-----------------------------------------------------------
 // emailPoubelle config
 // Licence : GNU GPL v3 : http://www.gnu.org/licenses/gpl.html
-// Créateur : David Mercereau - david [.] mercereau [aro] zici [.] fr
+// CrÃ©ateur : David Mercereau - david [aro] mercereau [.] info
 // Home : http://poubelle.zici.fr
 //----------------------------------------------------------- 
 
-error_reporting(0);
+//error_reporting(0);
+define('DEBUG', true);
 
-define('VERSION', '0.3');
+
+
 
 // Domaine email
 define('DOMAIN', 'zici.fr');
 
-// Serveur DNS pour la résolution/vérification du nom de domaine
-define('NS1', 'ns1.fdn.org');
-define('NS2', '8.8.8.8');
+// Deux options : 
+// 		PLAIN : plaintext, pas de base, simple mais des fonctionnalitÃ©s en moins
+//		DB : pdo usage
+define('BACKEND', 'DB');
 
-define('DEBUG', false);
+if (BACKEND == 'DB') {
+    // PDO stucture
+    // Exemple pour MYSQL : 
+    //      define('DB', 'mysql:host=127.0.0.1;dbname=baseMysql');
+    //      define('DBUSER', 'utilisateurMysql');
+    //      define('DBPASS', 'motdepassedefou');
+    // Exemple pour Sqlite : 
+    //      define('DB', 'sqlite:./data/emailPoubelle.sqlite');
+    define('DB', 'mysql:host=localhost;dbname=c1_demo');
+    #define('DB', 'sqlite:./database.sdb');
+    define('DBUSER', 'c1_demo');
+    define('DBPASS', 'sqdf2csd4rvn45548');
+}
 
 // Fichier d'alias postfix
-define('FICHIERALIAS', './emailPoubelle/postfix/virtual');
+define('FICHIERALIAS', './data/virtual');
 define('BIN_POSTMAP', '/usr/sbin/postmap');
 
-define('URLPAGE', 'http://www.zici.fr/emailPoubelle.html');
+define('URLPAGE', 'http://'.$_SERVER["SERVER_NAME"].'/'.$_SERVER["REQUEST_URI"]);
 
 // A indiquer si vous utiliser les URL's rewriting
 // Exemple avec un htaccess
-// 		RewriteRule ^EmailPoubell-([0-9]+)\.html$  index.php?page=emailPoubelle&Validemail=$1  [L]
-define('URLREWRITE_DEBUT', 'http://www.zici.fr/EmailPoubell-');
-define('URLREWRITE_FIN', '.html');
-// Désactiver
-# define('URLREWRITE_DEBUT', false);
-# define('URLREWRITE_FIN', false);
+// 		RewriteRule ^EmailPoubell-([0-9]+)\.html$  folder/emailPoubelle.php?&Validemail=$1  [L]
+//define('URLREWRITE_DEBUT', 'http://www.zici.fr/EmailPoubell-');
+//define('URLREWRITE_FIN', '.html');
+// DÃ©sactiver
+define('URLREWRITE_DEBUT', false);
+define('URLREWRITE_FIN', false);
 
 // - Email 
 // Sujet de l'email pour la confirmation
-define('EMAIL_SUJET_CONFIRME', '[zici] [EmailPoubelle] Confirmation alias ');
+define('EMAIL_SUJET_CONFIRME', '[EmailPoubelle] Confirmation alias ');
 // Sujet de l'email pour la liste des alias
-define('EMAIL_SUJET_LISTE', '[zici] [EmailPoubelle] Liste des alias ');
+define('EMAIL_SUJET_LISTE', '[EmailPoubelle] Liste des alias ');
 // From de l'email
-define('EMAIL_FROM', '"NO REPLAY emailPoubelle" <emailpoubelle@zici.fr>');
+define('EMAIL_FROM', '"NO REPLAY emailPoubelle" <emailpoubelle@exemple.com>');
 
-// Alisas interdit : (regex ligne par ligne) - commenter pour désactiver
-define('ALIASDENY', './emailPoubelle/aliasdeny.txt');
+// Alisas interdit : (regex ligne par ligne) - commenter pour dÃ©sactiver
+define('ALIASDENY', './aliasdeny.txt');
 
-// Blackliste d'email : (regex ligne par ligne) - commenter pour désactiver
-define('BLACKLIST', './emailPoubelle/blacklist.txt');
+// Blackliste d'email : (regex ligne par ligne) - commenter pour dÃ©sactiver
+define('BLACKLIST', './blacklist.txt');
+
+// Depend pear Net/DNS2
+define('CHECKMX', false);
+if (CHECKMX) {
+    require_once('Net/DNS2.php');
+    // Serveur DNS pour la rÃ©solution/vÃ©rification du nom de domaine
+    define('NS1', 'ns1.fdn.org');
+    define('NS2', '8.8.8.8');
+}
 
 ?>
--- emailPoubelle.php
+++ emailPoubelle.php
@@ -3,51 +3,100 @@
 //-----------------------------------------------------------
 // Email Poubelle
 // Licence : GNU GPL v3 : http://www.gnu.org/licenses/gpl.html
-// Créateur : David Mercereau - david [.] mercereau [aro] zici [.] fr
+// CrÃ©ateur : David Mercereau - david [aro] mercereau [.] info
 // Home : http://poubelle.zici.fr
 // Date : 08/2013
-// Version : 0.3
-// Dépendance : Postifx
+// Version : 0.4
+// DÃ©pendance : Postifx
 //----------------------------------------------------------- 
 
 include_once('./conf.php');
+define('VERSION', '0.4');
 
-# Init & vérif
+//////////////////
+// Init & check
+//////////////////
+
+if (DEBUG) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+	echo '<div class="highlight-2">Debug activÃ©</div>';
+    print_r($_REQUEST);
+}
+
+// check alias file is_writable 
 if (!is_writable(FICHIERALIAS)) {
-	exit('<div class="highlight-1">Erreur : le fichier d\'alias ne peut pas être écrit. Merci de contacter l\'administrateur</div>');
+	exit('<div class="highlight-1">Erreur : le fichier d\'alias ne peut pas Ãªtre Ã©crit. Merci de contacter l\'administrateur</div>');
 }
+// check blacklist file is_writable
 if (defined('BLACKLIST') && !is_readable(BLACKLIST)) {
-    exit('<div class="highlight-1">Erreur : un fichier de blacklist est renseigné mais n\'est pas lisible. Merci de contacter l\'administrateur</div>');
+    exit('<div class="highlight-1">Erreur : un fichier de blacklist est renseignÃ© mais n\'est pas lisible. Merci de contacter l\'administrateur</div>');
 }
+// check aliasdeny file is_writable
 if (defined('ALIASDENY') && !is_readable(ALIASDENY)) {
-    exit('<div class="highlight-1">Erreur : un fichier d\'alias interdit est renseigné mais n\'est pas lisible. Merci de contacter l\'administrateur</div>');
+    exit('<div class="highlight-1">Erreur : un fichier d\'alias interdit est renseignÃ© mais n\'est pas lisible. Merci de contacter l\'administrateur</div>');
 }
 
+// Connect DB
+if (BACKEND == 'DB') {
+    try {
+        if (preg_match('/^sqlite/', DB)) {
+            $dbco = new PDO(DB);
+        } else {
+            $dbco = new PDO(DB, DBUSER, DBPASS);
+        }
+        $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch ( PDOException $e ) {
+        die('Connexion Ã  la base '.$e->getMessage());
+    }
+}
+
+//////////////////
+// Function 
+//////////////////
+
+// Verification des emails
 function VerifMXemail($email) {
-	require_once 'Net/DNS2.php';
-	$domaine=explode('@', $email);
-	$r = new Net_DNS2_Resolver(array('nameservers' => array(NS1, NS2)));
-	try {
-        $result = $r->query($domaine[1], 'MX');
-	} catch(Net_DNS2_Exception $e) {
-		return false;
-	}
-	if ($result->answer) {
-		return true;
-	} else {
-		return false;
-	}
+    if (CHECKMX) {
+    	$domaine=explode('@', $email);
+    	$r = new Net_DNS2_Resolver(array('nameservers' => array(NS1, NS2)));
+    	try {
+            $result = $r->query($domaine[1], 'MX');
+    	} catch(Net_DNS2_Exception $e) {
+    		return false;
+    	}
+    	if ($result->answer) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    } else {
+        return true;
+    }
 }
 
+// postmap command
 function UpdateVirtualDB() {
 	echo exec(BIN_POSTMAP.' '.FICHIERALIAS,$output,$return);
 }
+
+//// A FAIRE (chian) :
+
+// add new alias
 function AjouterAlias($alias,$email) {
-	$fichier=fopen(FICHIERALIAS,'a+'); 
-	fputs($fichier, $alias.'@'.DOMAIN.' '.$email."\n");
-	fclose($fichier);
+    if (BACKEND == 'DB') {
+        // A faire mais $alais doÃ®t changer
+    } else {
+	    $fichier=fopen(FICHIERALIAS,'a+'); 
+	    fputs($fichier, $alias.'@'.DOMAIN.' '.$email."\n");
+	    fclose($fichier);
+    }
 	UpdateVirtualDB();
 }
+
+//// A FAIRE :
+
+// delete new alias
 function SupprimerAlias($alias,$email) {
 	file_put_contents(FICHIERALIAS, preg_replace('#\n\#[0-9]+ '.$alias.'@'.DOMAIN.' '.$email.'#U', '', file_get_contents(FICHIERALIAS)/*, 1*/));
 	file_put_contents(FICHIERALIAS, preg_replace('#\n'.$alias.'@'.DOMAIN.' '.$email.'#U', '', file_get_contents(FICHIERALIAS)/*, 1*/));
@@ -55,6 +104,7 @@ function SupprimerAlias($alias,$email) {
 	UpdateVirtualDB();
 }
 
+// parse file for blacklist and aliasdeny
 function parseFileRegex($file, $chaine) {
     $return=false;
     $handle = fopen($file, 'r');
@@ -72,6 +122,7 @@ function parseFileRegex($file, $chaine) {
     return $return;
 }
 
+// Check blacklistemail
 function BlacklistEmail($email) {
     if (defined('BLACKLIST')) {
         return parseFileRegex(BLACKLIST, $email);
@@ -80,6 +131,7 @@ function BlacklistEmail($email) {
     }
 }
 
+// check aliasdeny
 function AliasDeny($alias) {
     if (defined('ALIASDENY')) {
         return parseFileRegex(ALIASDENY, $alias);
@@ -88,6 +140,9 @@ function AliasDeny($alias) {
     }
 }
 
+//// A FAIRE :
+
+// list alias 
 function ListeAlias($email) {
     $handle = fopen(FICHIERALIAS, 'r');
     while (!feof($handle)) {
@@ -103,14 +158,11 @@ function ListeAlias($email) {
     return ($alias);
 }
 
-if (DEBUG) {
-	echo '<div class="highlight-2">Debug activé</div>';
-    print_r($_REQUEST);
-}
+//////////////////
+// Start program
+//////////////////
 
-echo '<h1>Emails poubelle libre</h1>
-<p>Générer des emails poubelle sans contrainte de durée de vie. </p>';
-
+// Valid email process
 if (isset($_REQUEST['Validemail'])) {
 	if (preg_match('/#'.$_REQUEST['Validemail'].' [a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+ [a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+/', file_get_contents(FICHIERALIAS),$res)) {
 		$data=explode(' ', $res[0]);
@@ -123,6 +175,7 @@ if (isset($_REQUEST['Validemail'])) {
 	} else {
 		echo '<div class="highlight-1">Erreur : ID introuvable</div>';
 	}
+// list email process
 } elseif (isset($_REQUEST['list'])) {
     $email=strtolower($_REQUEST['email']);
     if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -132,7 +185,7 @@ if (isset($_REQUEST['Validemail'])) {
     } else if (!preg_match('#\n[a-z0-9]+@'.DOMAIN.' '.$email.'#', file_get_contents(FICHIERALIAS))) {
         echo '<div class="highlight-1">Vous n\'avez encore aucun alias d\'actif</div>';
      } else {
-        # Envoi de l'email récapitulatif : 
+        # send email with alias list 
         if (!preg_match('#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#', $email)) {
             $passage_ligne = "\r\n";
         } else {
@@ -147,8 +200,9 @@ if (isset($_REQUEST['Validemail'])) {
         $message.= 'Pour supprimer un email poubelle vous pouvez vous rendre sur le lien ci-dessou : '.$passage_ligne;
         $message.= "\t * ".URLPAGE.$passage_ligne;
         mail($email,EMAIL_SUJET_LISTE,$message,$header);
-        echo '<div class="highlight-3">Un email vous a été adressé avec la liste de vos emails poubelles actifs.</div>';
+        echo '<div class="highlight-3">Un email vous a Ã©tÃ© adressÃ© avec la liste de vos emails poubelles actifs.</div>';
     }
+// 
 } elseif (isset($_REQUEST['email']) && isset($_REQUEST['alias'])) {
 	$alias=strtolower($_REQUEST['alias']);
 	$email=strtolower($_REQUEST['email']);
@@ -161,10 +215,10 @@ if (isset($_REQUEST['Validemail'])) {
 	} else if (AliasDeny($alias)) {
 		echo '<div class="highlight-1">Erreur : email poubelle interdit</div>';
     } else if (BlacklistEmail($email)) {
-        echo '<div class="highlight-1">Erreur : vous avez été blacklisté sur ce service</div>';
-	} elseif (isset($_REQUEST['ajo'])) {
+        echo '<div class="highlight-1">Erreur : vous avez Ã©tÃ© blacklistÃ© sur ce service</div>';
+	} elseif (isset($_REQUEST['add'])) {
         if (preg_match('#\n'.$alias.'@'.DOMAIN.'#', file_get_contents(FICHIERALIAS)) || preg_match('#\n\#[0-9]+ '.$alias.'@'.DOMAIN.'#', file_get_contents(FICHIERALIAS))) {
-			echo '<div class="highlight-1">Erreur : cet email poubelle est déjà utilisé</div>';
+			echo '<div class="highlight-1">Erreur : cet email poubelle est dÃ©jÃ  utilisÃ©</div>';
 		} else {
 			if (preg_match('#\n[a-z0-9]+@'.DOMAIN.' '.$email.'#', file_get_contents(FICHIERALIAS))) {
 				AjouterAlias($alias,$email);
@@ -172,7 +226,7 @@ if (isset($_REQUEST['Validemail'])) {
 			} else {
 				$id=rand().date('U');
 				$alias_desactive='#'.$id.' '.$alias;
-				AjouterAlias($alias_desactive,$email);
+				AjouterAlias($alias_desactive,$email,0);
 				# Envoi de l'email : 
 				if (!preg_match('#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#', $email)) {
 					$passage_ligne = "\r\n";
@@ -181,7 +235,7 @@ if (isset($_REQUEST['Validemail'])) {
 				}
 				$header = 'From: '.EMAIL_FROM.$passage_ligne;
 				$header.= 'MIME-Version: 1.0'.$passage_ligne;
-				$message= 'Confirmation de la création de votre redirection email poubelle : '.$passage_ligne;
+				$message= 'Confirmation de la crÃ©ation de votre redirection email poubelle : '.$passage_ligne;
 				$message= $alias.'@'.DOMAIN.' => '.$email.$passage_ligne;
 				$message= 'Cliquer sur le lien ci-dessous pour confirmer : '.$passage_ligne;
 				if (URLREWRITE_DEBUT && URLREWRITE_FIN) {
@@ -196,28 +250,35 @@ if (isset($_REQUEST['Validemail'])) {
 					$message.= "\t * ".URLPAGE.'?sup=true&email='.$email.'&alias='.$alias.$passage_ligne;
 				}
 				mail($email,EMAIL_SUJET_CONFIRME.$alias,$message,$header);
-				echo '<div class="highlight-2">Votre email ('.$email.') nous étant inconnu, une confirmation vous a été envoyé par email.</div>';
+				echo '<div class="highlight-2">Votre email ('.$email.') nous Ã©tant inconnu, une confirmation vous a Ã©tÃ© envoyÃ© par email.</div>';
 			}
 		}
-	} else if (isset($_REQUEST['sup'])) {
+	} else if (isset($_REQUEST['del'])) {
 		if (preg_match('#\n'.$alias.'@'.DOMAIN.' '.$email.'#', file_get_contents(FICHIERALIAS)) || preg_match('#\n\#[0-9]+ '.$alias.'@'.DOMAIN.' '.$email.'#', file_get_contents(FICHIERALIAS))) {
 			SupprimerAlias($alias,$email);
-			echo '<div class="highlight-3">Votre email poubelle <b>'.$alias.'@'.DOMAIN.'</b> est maintenant supprimé !</div>';
+			echo '<div class="highlight-3">Votre email poubelle <b>'.$alias.'@'.DOMAIN.'</b> est maintenant supprimÃ© !</div>';
 		} else {
 			echo '<div class="highlight-1">Erreur : l\'email poubelle n\'existe pas</div>';
 		}
 	}
 }
+// Close connexion DB
+if (BACKEND == DB) {
+    $dbco = null;
+}
+//////////////////
+// Printing form
+//////////////////
 ?>
 <form action="<?= URLPAGE?>" method="post">
 <label>Nom de l'email poubelle : </label>
 <input type="text" name="alias" size="24" border="0" /> @<?= DOMAIN; ?>	
 <br />
-<label>Votre email réel : </label>
-<input type="text" name="email" size="24" border="0" /> <input class="button" type="submit" name="list" value="Lister" />
+<label>Votre email rÃ©el : </label>
+<input type="text" name="email" size="24" border="0" /> <input class="" type="submit" name="list" value="Lister" />
 <br />
-<input class="button" type="submit" name="ajo" value="Créer" /> ou
-<input class="button" type="submit" name="sup" value="Supprimer" /> la redirection poubelle
+<input class="button" type="submit" name="add" value="CrÃ©er" /> ou
+<input class="button" type="submit" name="del" value="Supprimer" /> la redirection poubelle
 </form>
-<p>Version <?= VERSION ?> - Créé par David Mercereau sous licence GNU GPL v3</p>
-<p>Télécharger et utiliser ce script sur le site du projet <a target="_blank" href="http://forge.zici.fr/p/emailpoubelle-php/">emailPoubelle.php</a></p>
+<p>Version <?= VERSION ?> - CrÃ©Ã© par David Mercereau sous licence GNU GPL v3</p>
+<p>TÃ©lÃ©charger et utiliser ce script sur le site du projet <a target="_blank" href="http://forge.zici.fr/p/emailpoubelle-php/">emailPoubelle.php</a></p>
